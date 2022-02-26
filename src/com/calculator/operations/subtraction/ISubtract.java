dbd0c0d44b8ad7e612b3e85f54734703c9cdf0cf
Wesley Tavares <91393560+WesleyTavaresDev@users.noreply.github.com>
2022-02-26 17:08:04 -0300

    ISubtract interface created with subtract default method together.
    Subtraction class created and implements ISubtract and subtract method

---
+++ src/com/calculator/operations/subtraction/ISubtract.java
@@ -0,0 +1,8 @@
+package src.com.calculator.operations.subtraction;
+
+public interface ISubtract {
+    
+    public default double subtract(double a, double b) {
+        return 0;
+    }
+}
---
+++ src/com/calculator/operations/subtraction/Subtraction.java
@@ -0,0 +1,9 @@
+package src.com.calculator.operations.subtraction;
+
+public class Subtraction implements ISubtract {
+
+    @Override
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+}
